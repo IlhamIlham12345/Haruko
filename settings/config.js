e33c0b21579b021f7a404c820ea653435c93f5a0
Hanuko official <hamham7887ilhamm@gmail.com>
2026-03-11 19:39:09 +0800

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,69 @@
+const settings = {
+  token: '7965748344:AAGvQy-GdgV3dHJs2ybjuVaF1vgQMRbht7c', // token dari @BotFather
+  
+  owner: 'naelishere', // jangan di ganti
+  ownerId: 7511061771, // user id
+  dev: 'haruko_dev', // wajib username tele
+  
+  noDana: '083874410982', // nomer dana
+  namaDana: 'ha**', // nama
+    
+  chUsn: '@titlenyaaja', // ch tele
+  
+  exPGroupId: "-1003588118874", // id group
+  exGroupId: "-1003588118874", // id group
+    
+  hostname: "naelvps", // hostname vps
+  cfApiToken: "oWm4SD2iMGwLk_MQryXVlOiiBovAlVQxtoADiet7", // api token cloudflare
+  cfZoneId: "1b662cae2a8214a8468c97fb552070d0", // zone id domain cf
+    
+  apiDigitalOcean: "dop_v1_10349213e7cca2c53347509934827fdabb6904be09bba290cf00b1f0dae6322c",   // apikey DO
+  apiDigitalOcean2: "",  // apikey DO 2  
+  apiDigitalOcean3: "",  // apikey DO 3
+  apiDigitalOcean4: "",  // apikey DO 4
+  apiDigitalOcean5: "",  // apikey DO 5
+  apiDigitalOcean6: "",  // apikey DO 6
+  apiDigitalOcean7: "",  // apikey DO 7
+  apiDigitalOcean8: "",  // apikey DO 8
+  apiDigitalOcean9: "",  // apikey DO 9
+  apiDigitalOcean10: "", // apikey DO 10
+  
+  pp: 'https://files.catbox.moe/th4v73.mp4', // file catbox foto
+  ppVid: 'https://files.catbox.moe/3n1i0s.mp4', // file catbox video
+  panel: 'https://files.catbox.moe/iir554.jpg', // foto panel
+
+  qris: 'https://files.catbox.moe/iir554.jpg', // qris
+
+  domain: 'https://hacker.nezprivate.my.id', // domain public
+  plta: 'ptla_RoEFGYEv3TYskIXISYNMGSFA6HgIANCeQE0hVPidzDZ', // ptla panel
+  pltc: 'ptlc_VSdmuf54sc7JapuRY4dUKyt8YBU4kst3PSLc6RiyZtq', // ptlc panel
+    
+  domainPriv: '', // domain private
+  pltaPriv: '', // apikey ptla
+  pltcPriv: '', // apikey ptlc
+    
+    // PANEL SERVER 2
+  domainV2: '', // domain v2
+  pltaV2: '', // plta v2
+  pltcV2: '', // ptlc v2
+    
+    // PANEL SERVER 3
+  domainV3: '', // domain v3 
+  pltaV3: '', // ptla v3
+  pltcV3: '', // ptlc v3
+    
+    // PANEL SERVER 4
+  domainV4: '', // domain v4
+  pltaV4: '', // ptla v4
+  pltcV4: '', // ptlc v4
+    
+    // PANEL SERVER 5
+  domainV5: '', // domain v5
+  pltaV5: '', // ptla v5
+  pltcV5: '', // ptlc v5
+  
+  loc: '1', // location id
+  eggs: '15' // egg id
+};
+
+module.exports = settings;
